db67b680ee5c7b1abf5ce510d53df2eb81452200
Sina <sina.sultani@semcon.com>
2023-03-16 13:32:47 +0100

    Working w/o Vector Icons

--- android/app/src/main/java/com/project_luna/MainActivity.java
+++ android/app/src/main/java/com/project_luna/MainActivity.java
@@ -1,6 +1,9 @@
 package com.project_luna;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
 import android.os.Bundle;
 
@@ -14,7 +17,22 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "project_luna";
   }
-
+  /**
+   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
+   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
+   * (aka React 18) with two boolean flags.
+   */
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new DefaultReactActivityDelegate(
+        this,
+        getMainComponentName(),
+        // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+        DefaultNewArchitectureEntryPoint.getFabricEnabled(), // fabricEnabled
+        // If you opted-in for the New Architecture, we enable Concurrent React (i.e. React 18).
+        DefaultNewArchitectureEntryPoint.getConcurrentReactEnabled() // concurrentRootEnabled
+        );
+  }
   //To avoid crashed caused by react-native-screens
   @Override
 protected void onCreate(Bundle savedInstanceState) {
--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import { Fonts, Provider as PaperProvider, DefaultTheme, configureFonts } from '
 import App from './App';
 import { name as appName } from './app.json';
 
+
 export default function Main() {
     return (
         <PaperProvider>
